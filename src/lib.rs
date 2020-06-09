ee5a9b6c10d330a77247fae468e310d50ac5f63a
Nelson Torres <ntorressm@gmail.com>
2020-06-08 20:31:38 -0400

    Obtains minecraft folder

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,21 @@
+extern crate dirs;
+
+use std::path::{Path, PathBuf};
+
 pub fn run() {
     println!("Hello, world!");
+}
+
+fn get_minecraft_folder_path() -> PathBuf {
+    dirs::home_dir().expect("Could not get home directory").join(".minecraft")
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-
+    #[test]
+    fn minecraft_folder_exists() {
+        assert!(get_minecraft_folder_path().as_path().is_dir(), "Minecraft folder does not exists");
+    }
 }
