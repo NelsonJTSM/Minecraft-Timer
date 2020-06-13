4d1785a72c4555dbbca33c89b964f27cc973afb1
Nelson Torres <ntorressm@gmail.com>
2020-06-13 02:33:12 -0400

    Fixed warnings of unused code

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,13 @@ extern crate notify;
 
 use std::fs;
 use std::path::PathBuf;
+use std::sync::mpsc::channel;
+use std::time::Duration;
+use std::thread;
 
 use serde::{Deserialize, Serialize};
 
-use notify::{DebouncedEvent, INotifyWatcher, RecommendedWatcher, RecursiveMode, Watcher};
-use std::sync::mpsc::channel;
-use std::sync::mpsc::Receiver;
-use std::time::Duration;
-
-use std::thread;
+use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
 
 pub fn run() {
     let minecraft_folder = get_minecraft_folder_path();
@@ -20,7 +18,8 @@ pub fn run() {
     let stat_thread = thread::spawn(|| {
         let (tx, rx) = channel();
         let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2)).unwrap();
-        watcher.watch(saves_folder, RecursiveMode::Recursive);
+        watcher.watch(saves_folder, RecursiveMode::Recursive).unwrap();
+
         loop {
             match rx.recv() {
                 Ok(event) => match event {
@@ -39,79 +38,6 @@ pub fn run() {
     });
 
     stat_thread.join().unwrap();
-
-}
-
-fn loop_new_player_watcher(receiver: &Receiver<DebouncedEvent>, watcher: &mut INotifyWatcher) {
-    match receiver.recv() {
-        Ok(event) => match event {
-            DebouncedEvent::Create(new_player_stat) => {
-                println!("new_player_stat: {:?}", new_player_stat);
-                watcher
-                    .watch(new_player_stat, RecursiveMode::NonRecursive)
-                    .unwrap();
-            }
-            e => println!("new_player_watcher: {:?}", e),
-        },
-        Err(e) => println!("watch error: {:?}", e),
-    }
-}
-
-fn loop_new_world_watcher(receiver: &Receiver<DebouncedEvent>, watcher: &mut INotifyWatcher) {
-    match receiver.recv() {
-        Ok(event) => match event {
-            DebouncedEvent::Create(new_world_directory) => {
-                println!("new world made! {:?}", new_world_directory);
-                println!(
-                    "{:?}",
-                    watcher
-                        .watch(new_world_directory, RecursiveMode::NonRecursive)
-                        .unwrap()
-                );
-            }
-            _ => (),
-        },
-        Err(e) => println!("watch error: {:?}", e),
-    }
-}
-
-fn loop_stat_folder_watcher(receiver: &Receiver<DebouncedEvent>, watcher: &mut INotifyWatcher) {
-    match receiver.recv() {
-        Ok(event) => {
-            println!("event: {:?}", event);
-
-            match event {
-                DebouncedEvent::Create(new_file) => {
-                    println!("new stat folder? {:?}", new_file);
-                    if new_file.file_name().unwrap() == "stats" {
-                        println!("new stat folder {:?}", new_file);
-                        watcher
-                            .watch(new_file, RecursiveMode::NonRecursive)
-                            .unwrap();
-                    }
-                }
-                e => println!(".....: {:?}", e),
-            }
-        }
-        Err(e) => println!("watch error: {:?}", e),
-    }
-}
-
-fn loop_stat_watcher(receiver: &Receiver<DebouncedEvent>) {
-    match receiver.recv() {
-        Ok(event) => match event {
-            DebouncedEvent::Write(player_stat) => {
-                println!("loop_stat_watcher {:?}", player_stat);
-
-                let player_stat_json =
-                    fs::read_to_string(player_stat).expect("Error trying to read file");
-
-                display_player_stat(player_stat_json);
-            }
-            e => println!("loop_stat_watcher {:?}", e),
-        },
-        Err(e) => println!("watch error: {:?}", e),
-    }
 }
 
 fn display_player_stat(player_json: String) {
